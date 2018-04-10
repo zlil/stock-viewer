9009ea217688301543fe6f8f003ea863bbefa114
zlil <zlil.korman@gmail.com>
2018-04-11 01:25:24 +0300

    fix charts

--- src/components/Stock.vue
+++ src/components/Stock.vue
@@ -9,6 +9,7 @@
       <div class="row">
         <div class="col s12 m12 l12 xl12">
           <div ref="graph"></div>
+          <div ref="graph2"></div>
         </div>
       </div>
     </div>
@@ -18,11 +19,11 @@
 <script>
 
   export default {
-    props: ['id', 'stockData', 'lastUpdate'],
+    props: ['id', 'stockData', 'heading'],
     async mounted() {
 
       this.keys = Object.keys(this.stockData['data']['Time Series (1min)']);
-      this.heading = this.stockData ? this.stockData['data']['Meta Data']['2. Symbol'] : ''
+      //this.heading = this.stockData ? this.stockData['data']['Meta Data']['2. Symbol'] : ''
       this.currentStockValue = this.stockData['data']['Time Series (1min)'][this.keys[0]]['4. close']
       let self = this
       $(document).ready(async () => {
@@ -30,9 +31,25 @@
         this.graph = self.drawGraph({
           title: 'Stock Movement',
           renderTo: self.$refs.graph,
+          categories: self.graphData.categories,
           series: [{
             name: 'Price',
-            data: self.graphData,
+            data: self.graphData.prices,
+            color: '#e57373',
+            pointStart: new Date(this.keys[0]).getTime(),
+            pointInterval: (36 * 10000) / 6,
+            tooltip: {
+              valuePrefix: 'Stock Value: '
+            },
+          }],
+        });
+        this.graph2 = self.drawGraph2({
+          title: 'Stock Movement',
+          renderTo: self.$refs.graph2,
+          categories: self.graphData.categories,
+          series: [{
+            name: 'Price',
+            data: self.graphData.prices,
             color: '#e57373',
             pointStart: new Date(this.keys[0]).getTime(),
             pointInterval: (36 * 10000) / 6,
@@ -46,11 +63,12 @@
     },
     data() {
       return {
-        heading: null,
+        //heading: null,
         currentStockValue: null,
         currentTime: moment().format("YYYY-MM-DD HH:mm:ss"),
         trendClass: null,
         graph: null,
+        graph2: null,
         graphData: [],
         keys: [],
         trend: null
@@ -70,44 +88,103 @@
       }
     },
     methods: {
-      drawGraph(options) {
+      drawGraph2(options) {
 
         let self = this
 
         return new Highcharts.stockChart(options.renderTo, {
-          chart: {borderColor: '#eeeeee', borderWidth: 2},
-          title: {text: options.title},
+//          chart: {borderColor: '#eeeeee', borderWidth: 2},
+//          title: {text: options.title},
+//          xAxis: {
+//            title: {text: '1 minute interval'}, type: 'datetime', dateTimeLabelFormats: {
+//              second: '%Y-%m-%d<br/>%H:%M:%S',
+//              minute: '%Y-%m-%d<br/>%H:%M'
+//            }
+//          },
+//          yAxis: {title: {text: `${self.heading} Price`, margin: 80}},
+//          plotOptions: {spline: {marker: {symbol: 'dot'}}},
+//          tooltip: {shared: false, hideDelay: 0},
+//          series: options.series || null,
+//          animation: false,
+//          exporting: false,
+//          credits: false,
+//          rangeSelector: {
+//            selected: 1
+//          }
           xAxis: {
-            title: {text: '1 minute interval'}, type: 'datetime', dateTimeLabelFormats: {
-              second: '%Y-%m-%d<br/>%H:%M:%S',
-              minute: '%Y-%m-%d<br/>%H:%M'
+            allowDecimals: true,
+            categories: options.categories,
+            type: 'datetime'
+          },
+
+          plotOptions: {
+            series: {
+              pointStart: 100
             }
           },
-          yAxis: {title: {text: `${self.heading} Price`, margin: 80}},
-          plotOptions: {spline: {marker: {symbol: 'dot'}}},
-          tooltip: {shared: false, hideDelay: 0},
-          series: options.series || null,
-          animation: false,
-          exporting: false,
-          credits: false,
-          rangeSelector: {
-            selected: 1
-          }
 
+          series: options.series || null,
+        })
+
+      },
+      drawGraph(options) {
+
+        let self = this
+
+        return new Highcharts.chart(options.renderTo, {
+          xAxis: {
+            allowDecimals: true,
+            categories: options.categories,
+            type: 'datetime'
+          },
+
+          plotOptions: {
+            series: {
+              pointStart: 100
+            }
+          },
+
+          series: options.series || null,
         })
       },
       fetchSixHourData() {
-
+        let lastDataSignatureAt = null;
         let sixHoursBack = moment(this.keys[0]).subtract(6, 'hours').format("YYYY-MM-DD HH:mm:00")
+        let lastDataSegment = this.keys[0];
+        if(lastDataSegment < this.currentTime)
+           lastDataSignatureAt = lastDataSegment
+        else
+          lastDataSignatureAt = this.currentTime
         let prices = [];
-        while (sixHoursBack < '2018-04-06 16:00:00') {
+        let categories = [];
+        let j = 0;
+        debugger
+        while (sixHoursBack < lastDataSignatureAt) {
           let price = this.stockData['data']['Time Series (1min)'][sixHoursBack] ? this.stockData['data']['Time Series (1min)'][sixHoursBack]['4. close'] : prices[prices.length - 1];
-          prices.push([sixHoursBack, parseFloat(price)])
+          if (price != undefined && sixHoursBack != undefined)
+            prices.push(parseFloat(price))
+          if (j / 15 == 0) {
+            categories.push("" + `${sixHoursBack}` + "")
+          }
           sixHoursBack = moment(sixHoursBack).add(1, 'minutes').format("YYYY-MM-DD HH:mm:00")
         }
-        this.graphData = prices
+        this.graphData.prices = prices
+        this.graphData.categories = categories
       }
-
     }
+//      fetchSixHourData() {
+//
+//        let sixHoursBack = moment(this.keys[0]).subtract(6, 'hours').format("YYYY-MM-DD HH:mm:00")
+//        let prices = [];
+//        while (sixHoursBack < this.currentTime) {
+//          let price = this.stockData['data']['Time Series (1min)'][sixHoursBack] ? this.stockData['data']['Time Series (1min)'][sixHoursBack]['4. close'] : prices[prices.length - 1];
+//          if(price != undefined && sixHoursBack != undefined)
+//            prices.push(parseFloat(price))
+//          sixHoursBack = moment(sixHoursBack).add(1, 'minutes').format("YYYY-MM-DD HH:mm:00")
+//        }
+//        this.graphData = prices
+//      }
+//
   }
+
 </script>
--- src/components/StocksContainer.vue
+++ src/components/StocksContainer.vue
@@ -6,13 +6,13 @@
     <p v-if="loading">Retrieving Information, Please Wait...</p>
     <div v-show="!loading">
       <p v-show="this.stocksData">{{this.getMeta}}</p>
-      <p v-show="this.error" deep-red>Error Occurred: {{ this.error }}</p>
+      <p v-show="this.error" class="red-text">Error Occurred: {{ this.error }}</p>
     </div>
     <div class="center-align stocks">
       <template v-for="(stock, index) in stocksDataElements">
         <div class="row">
           <component ref="stockElement" :is="stock" :key="index"
-                     v-bind="{id: index, stockData: stocksData[index]}"></component>
+                     v-bind="{id: index, stockData: stocksData[index], heading: stocksData[index]['data']['Meta Data']['2. Symbol']}"></component>
         </div>
       </template>
     </div>
@@ -26,33 +26,43 @@
   export default {
 
     async mounted() {
-
+      try {
         await axios.all(this.links.map(link => axios.get(link)))
           .then(axios.spread((...result) => {
             result.map(res => {
-              this.stocksDataElements.push(Stock)
+              if(res == null || res.data["Error Message"] != undefined)
+                throw '400'
+              else {
+                this.stocksDataElements.push(Stock)
+                this.stocksData.push(res)
+              }
             })
-            this.stocksData = result
+
             this.loading = false
           }))
           .catch(err => {
+            debugger
             this.error = err
             this.loading = false
           })
+      }catch(e){
+        debugger
+        console.log('error', e);
+      }
     },
     data() {
       return {
-        stocksData: null,
+        stocksData: [],
         stocksDataElements: [],
         error: null,
         loading: true,
         apiKey: '1FCYOMNM3HBVNJ4H',
         links: [
-          `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&outputsize=full&symbol=DJI&interval=1min&apikey=${this.apiKey}`,
-          `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&outputsize=full&symbol=INX&interval=1min&apikey=${this.apiKey}`,
-          `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&outputsize=full&symbol=NDX&interval=1min&apikey=${this.apiKey}`,
-          `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&outputsize=full&symbol=AMZN&interval=1min&apikey=${this.apiKey}`,
-          `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&outputsize=full&symbol=GOOGL&interval=1min&apikey=${this.apiKey}`]
+          "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&outputsize=full&symbol=DJI&interval=1min&apikey=1FCYOMNM3HBVNJ4H",
+          "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&outputsize=full&symbol=INX&interval=1min&apikey=1FCYOMNM3HBVNJ4H",
+          "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&outputsize=full&symbol=NDX&interval=1min&apikey=1FCYOMNM3HBVNJ4H",
+          "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&outputsize=full&symbol=AMZN&interval=1min&apikey=1FCYOMNM3HBVNJ4H",
+          "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&outputsize=full&symbol=GOOGL&interval=1min&apikey=1FCYOMNM3HBVNJ4H"]
       }
     },
     computed: {
